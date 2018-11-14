06281c47c4e11e7df63a1f155a7be25d0115f856
serubi <serubi@users.noreply.github.com>
2018-11-14 10:38:57 +0100

    Update sensehat2.py

--- sensehat2.py
+++ sensehat2.py
@@ -1,28 +1,10 @@
 from sense_hat import SenseHat
+import requests
 
 sense = SenseHat()
 
-# Display the letter J
-sense.show_letter("J")
 
-while True:
-	acceleration = sense.get_accelerometer_raw()
-	x = acceleration['x']
-	y = acceleration['y']
-	z = acceleration['z']
+response = requests.get('https://jsonplaceholder.typicode.com/todos/1')
+data = response.json()
 
-	x=round(x, 0)
-	y=round(y, 0)
-	z=round(z, 0)
-	
-	print("x={0}, y={1}, z={2}".format(x, y, z))
-
-  # Update the rotation of the display depending on which way up the Sense HAT is
-	if x  == -1:
-	  sense.set_rotation(180)
-	elif y == 1:
-	  sense.set_rotation(90)
-	elif y == -1:
-	  sense.set_rotation(270)
-	else:
-	  sense.set_rotation(0)
+print(data)
