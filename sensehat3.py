b7ff177397029c9a6a0e8b2612044695bbcd180a
serubi <serubi@users.noreply.github.com>
2018-11-14 13:40:51 +0100

    Create sensehat3.py

---
+++ sensehat3.py
@@ -0,0 +1,9 @@
+from sense_hat import SenseHat
+
+sense = SenseHat()
+orientation = sense.get_orientation_degrees()
+print("p: {pitch}, r: {roll}, y: {yaw}".format(**orientation))
+
+while True:
+  sense.show_letter(str(orientation))
+  time.sleep(0.1)
