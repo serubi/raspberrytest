4e6b0bc63bd5472656a32d51789df7377d506f2e
serubi <serubi@users.noreply.github.com>
2018-11-14 10:35:26 +0100

    Update sensehat2.py

--- sensehat2.py
+++ sensehat2.py
@@ -1,36 +1,28 @@
-#!/usr/bin/python
-import sys
 from sense_hat import SenseHat
 
-# To get good results with the magnetometer you must first calibrate it using
-# the program in RTIMULib/Linux/RTIMULibCal
-# The calibration program will produce the file RTIMULib.ini
-# Copy it into the same folder as your Python code
-
-led_loop = [4, 5, 6, 7, 15, 23, 31, 39, 47, 55, 63, 62, 61, 60, 59, 58, 57, 56, 48, 40, 32, 24, 16, 8, 0, 1, 2, 3]
-
 sense = SenseHat()
-sense.set_rotation(0)
-sense.clear()
 
-prev_x = 0
-prev_y = 0
-
-led_degree_ratio = len(led_loop) / 360.0
+# Display the letter J
+sense.show_letter("J")
 
 while True:
-    dir = sense.get_compass()
-    dir_inverted = 360 - dir  # So LED appears to follow North
-    led_index = int(led_degree_ratio * dir_inverted)
-    offset = led_loop[led_index]
+	acceleration = sense.get_accelerometer_raw()
+	x = acceleration['x']
+	y = acceleration['y']
+	z = acceleration['z']
 
-    y = offset // 8  # row
-    x = offset % 8  # column
+	x=round(x, 0)
+	y=round(y, 0)
+	z=round(z, 0)
+	
+	print("x={0}, y={1}, z={2}".format(x, y, z))
 
-    if x != prev_x or y != prev_y:
-        sense.set_pixel(prev_x, prev_y, 0, 0, 0)
-
-    sense.set_pixel(x, y, 0, 0, 255)
-
-    prev_x = x
-    prev_y = y
+  # Update the rotation of the display depending on which way up the Sense HAT is
+	if x  == -1:
+	  sense.set_rotation(180)
+	elif y == 1:
+	  sense.set_rotation(90)
+	elif y == -1:
+	  sense.set_rotation(270)
+	else:
+	  sense.set_rotation(0)
