f2915f5344787d7ee2a7dccc43edc11cd4b45c21
serubi <serubi@users.noreply.github.com>
2018-11-14 11:35:38 +0100

    Update sensehat2.py

--- sensehat2.py
+++ sensehat2.py
@@ -5,23 +5,39 @@ import sys
 sense = SenseHat()
 
 apikey = "3g25yMcEXasRcGshlTxbCTupYLcHSg9i"
-city = "Copenhagen"
 
-print(sys.argv[1])
+try:
+  sys.argv[1]
+except NameError:
+  city = "Roskilde"
+else:
+  city = sys.argv[1]
+
+print("Finder vejr for " + city)
 
 location = requests.get('http://dataservice.accuweather.com/locations/v1/cities/search?apikey=' + apikey + '&q=' + city)
 locationdata = location.json()
-locationkey = locationdata[0]['Key']
-locationname = locationdata[0]['LocalizedName']
 
-weather = requests.get('http://dataservice.accuweather.com/currentconditions/v1/' + locationkey + '?apikey=' + apikey + '&q=' + city)
-weatherdata = weather.json()
-temptext = weatherdata[0]['WeatherText']
-temp = weatherdata[0]['Temperature']['Metric']['Value']
+try:
+  locationdata[0]['Key']
+except NameError:
+  red = (255, 0, 0)
+  white = (255, 255, 255)
 
-black = (0, 0, 0)
-white = (155, 155, 155)
+  while True:
+    sense.show_message('Byen "' + city + '" findes ikke', text_colour=white, back_colour=red, scroll_speed=0.05)
+else:
+  locationkey = locationdata[0]['Key']
+  locationname = locationdata[0]['LocalizedName']
 
-while True:
-  sense.show_message("Temperatur i " + locationname + ": " + str(temp) + "C" + " ... " + temptext, text_colour=white, back_colour=black, scroll_speed=0.05)
+  weather = requests.get('http://dataservice.accuweather.com/currentconditions/v1/' + locationkey + '?apikey=' + apikey + '&q=' + city)
+  weatherdata = weather.json()
+  temptext = weatherdata[0]['WeatherText']
+  temp = weatherdata[0]['Temperature']['Metric']['Value']
+
+  black = (0, 0, 0)
+  white = (155, 155, 155)
+
+  while True:
+    sense.show_message("Temperatur i " + locationname + ": " + str(temp) + "C" + " ... " + temptext, text_colour=white, back_colour=black, scroll_speed=0.05)
 
