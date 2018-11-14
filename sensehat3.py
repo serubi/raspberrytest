7baf99f5ccaa52f6a2971cf706b3acdb9e8be51c
serubi <serubi@users.noreply.github.com>
2018-11-14 13:54:31 +0100

    Update sensehat3.py

--- sensehat3.py
+++ sensehat3.py
@@ -3,9 +3,10 @@ import time
 import math
 
 sense = SenseHat()
-orientation = sense.get_orientation_degrees()
+#orientation = sense.get_orientation_degrees()
 #print("p: {pitch}, r: {roll}, y: {yaw}".format(**orientation))
 
 while True:
+  orientation = sense.get_orientation_degrees()
   sense.show_message(str(math.floor(orientation['yaw'])))
   #time.sleep(0.1)
