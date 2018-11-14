fd9f9348f5acaaf14c6ca6c547594521de8e041f
serubi <serubi@users.noreply.github.com>
2018-11-14 10:47:58 +0100

    Update sensehat2.py

--- sensehat2.py
+++ sensehat2.py
@@ -7,11 +7,11 @@ sense = SenseHat()
 response = requests.get('https://reqres.in/api/users/2')
 data = response.json()
 
-print(data["first_name"])
+#print(data["data"]["first_name"])
 
 black = (0, 0, 0)
 blue = (0, 0, 255)
 
 while True:
-  sense.show_message(data["first_name"] + " " + data["last_name"], text_colour=blue, back_colour=black, scroll_speed=0.05)
+  sense.show_message(data["data"]["first_name"] + " " + data["data"]["last_name"], text_colour=blue, back_colour=black, scroll_speed=0.05)
 
