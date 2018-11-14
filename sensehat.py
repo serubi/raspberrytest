26be4b5508d91194738ac57af24b0549298ab47a
serubi <serubi@users.noreply.github.com>
2018-11-14 13:21:29 +0100

    Update sensehat.py

--- sensehat.py
+++ sensehat.py
@@ -3,6 +3,23 @@ import time
 sense = SenseHat()
 sense.clear()
 
+def auto_rotate_display():
+  # read sensors data to detect orientation
+  x = round(sense.get_accelerometer_raw()['x'], 0)
+  y = round(sense.get_accelerometer_raw()['y'], 0)
+
+  rot = 0
+  if x == -1:
+    rot=90
+  elif y == -1:
+    rot=180
+  elif x == 1:
+    rot=270
+
+  # rotate the display according to the orientation
+  print ("Current orientation x=%s y=%s  rotating display by %s degrees" % (x, y, rot))
+  sense.set_rotation(rot)
+
 blue = (0, 0, 255)
 red = (255, 0, 0)
 white = (255, 255, 255)
@@ -36,8 +53,4 @@ for y in range(4, 8):
       sense.set_pixel(x, y, red)
 
 while True:
-  time.sleep(1)
-  sense.flip_v()
-  sense.flip_h()
-  time.sleep(1)
-  sense.flip_h()
+  auto_rotate_display()
