7d83a7e9b3be451bf3acd84eab49e0f92acebb82
serubi <serubi@users.noreply.github.com>
2018-11-14 10:45:35 +0100

    Update sensehat2.py

--- sensehat2.py
+++ sensehat2.py
@@ -4,7 +4,14 @@ import requests
 sense = SenseHat()
 
 
-response = requests.get('https://jsonplaceholder.typicode.com/todos/1')
+response = requests.get('https://reqres.in/api/users/2')
 data = response.json()
 
-print(data)
+#print(data[""])
+
+black = (0, 0, 0)
+blue = (0, 0, 255)
+
+while True:
+  sense.show_message(data["first_name"] + " " + data["last_name"], text_colour=blue, back_colour=black, scroll_speed=0.05)
+
