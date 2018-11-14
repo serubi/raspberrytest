5c8afe19d637fc1efdb4675307cb5e8e73912b8b
serubi <serubi@users.noreply.github.com>
2018-11-14 10:30:58 +0100

    Update sensehat2.py

--- sensehat2.py
+++ sensehat2.py
@@ -1,275 +1,228 @@
-import sense_hat, random, time
-
-"""
-
-  Rock Paper Scissors
-  
-  Play Rock Paper Scissors with your Raspberry Pi.  Select rock, paper or
-  scissors with the joystick. The Pi picks randomly.  
-  
-  Keeps track of your score- try best out of 5!
-  
-  Note: Requires sense_hat version 2.2.0 or later
-  
-"""
+import sense_hat
+from time import sleep, time
+from random import randint
 
 sense = sense_hat.SenseHat()
-
-game_state = { 
-          "comp_pick" : None,
-          "user_pick" : None,
-          "picks" : ("rock", "paper", "scissors"),
-          "choice_index" : 0,
-          "comp_score" : 0,
-          "user_score" : 0
-        }
-
-start_over_state = game_state.copy()
-
-Y = (255, 255, 0)
-G = (0, 255, 0)
-B = (0, 0, 255)
-X = (0, 0, 0)
-
-####
-# Game Images
-####
-
-big_rock = [
-    X, X, X, X, X, X, X, X,
-    X, X, X, X, X, X, X, X,
-    X, X, Y, Y, Y, X, X, X,
-    X, Y, Y, Y, Y, Y, X, X,
-    X, Y, Y, Y, X, Y, Y, X,
-    X, Y, Y, Y, Y, Y, Y, X,
-    X, X, Y, Y, Y, Y, X, X,
-    X, X, X, X, X, X, X, X
-]
-
-
-big_paper = [
-    X, X, X, X, X, X, X, X,
-    X, X, X, G, G, G, X, X,
-    X, X, G, X, G, G, X, X,
-    X, G, X, X, G, G, X, X,
-    X, G, G, G, G, G, X, X,
-    X, G, G, G, G, G, X, X,
-    X, G, G, G, G, G, X, X,
-    X, X, X, X, X, X, X, X
-]
-
-
-big_scissors = [
-    X, X, X, X, X, X, X, X,
-    X, B, X, X, X, X, B, X,
-    X, X, B, X, X, B, X, X,
-    X, X, X, B, B, X, X, X,
-    X, X, X, B, B, X, X, X,
-    X, B, B, X, X, B, B, X,
-    X, B, B, X, X, B, B, X,
-    X, X, X, X, X, X, X, X
-]
-
-
-user_rock = [
-    X, X, X, X, X, X, X, X,
-    X, X, X, X, X, Y, Y, X,
-    X, X, X, X, Y, Y, Y, Y,
-    X, X, X, X, Y, Y, Y, Y
-]
-
-user_paper = [
-    X, X, X, X, X, X, G, G,
-    X, X, X, X, X, G, X, G,
-    X, X, X, X, X, G, G, G,
-    X, X, X, X, X, G, G, G
-]
-
-user_scissors = [
-    X, X, X, X, B, X, X, B,
-    X, X, X, X, X, B, B, X,
-    X, X, X, X, X, B, B, X, 
-    X, X, X, X, B, X, X, B
-]
-
-comp_rock = [
-    X, X, X, X, X, X, X, X,
-    X, Y, Y, X, X, X, X, X,
-    Y, Y, Y, Y, X, X, X, X,
-    Y, Y, Y, Y, X, X, X, X
-]
-
-comp_paper = [
-    X, G, G, X, X, X, X, X,
-    G, X, G, X, X, X, X, X,
-    G, G, G, X, X, X, X, X,
-    G, G, G, X, X, X, X, X
-]
-
-comp_scissors = [
-    B, X, X, B, X, X, X, X,
-    X, B, B, X, X, X, X, X,
-    B, B, B, B, X, X, X, X,
-    B, X, X, B, X, X, X, X
-]
-     
-small_nothing = [
-    X, X, X, X, X, X, X, X,
-    X, X, X, X, X, X, X, X,
-    X, X, X, X, X, X, X, X,
-    X, X, X, X, X, X, X, X
-]
-
-####
-# Game Functions
-####
-
-def get_user_pick(state):
-    """
-    Takes game state and returns string of user's pick.
-    """
-    picks = state["picks"]
-    return picks[state["choice_index"] % len(picks)]
-
-def show_picks(state):
-
-    user = get_user_pick(state)
-    comp = state["comp_pick"]
-    
-    # show user pick, then comp pick
-    if user == "rock":
-        user_screen =  user_rock
-    elif user == "paper":
-        user_screen = user_paper
-    elif user == "scissors":
-        user_screen = user_scissors
-    if comp == "rock":
-        comp_screen = comp_rock
-    elif comp == "paper":
-        comp_screen = comp_paper
-    elif comp == "scissors":
-        comp_screen = comp_scissors
-    
-    # Display user choice
-    screen1 = small_nothing + user_screen
-    sense.set_pixels(screen1)
-    
-    time.sleep(.5)
-    
-    # Display comp choice too
-    screen2 = comp_screen + user_screen
-    sense.set_pixels(screen2)
-    
-    time.sleep(1)
-    
-    # Make alternate screen based on result
-    if user_wins(game_state):
-        P = (0, 150, 0)
-    elif user_wins(game_state) == None:
-        P = (100, 100, 100)
-    else:
-        P = (150, 0, 0)
-    screen3 = [P if i == X else i for i in screen2]
-    
-    # Flash Screen
-    for i in range(15):
-        sense.set_pixels(screen2)
-        time.sleep(.02)
-        sense.set_pixels(screen3)
-        time.sleep(.02)
-    
-def display_score(state):
-    user_score = state["user_score"]
-    comp_score = state["comp_score"]
-    sense.show_message("You: {0} Pi: {1}".format(user_score, comp_score))
-    display(state)
-
-def user_wins(state):
-    """
-    Returns True if the user wins, False if user loses, and None if tie
-    """
-    comp = state["comp_pick"]
-    user = state["user_pick"]
-    
-    if comp == user:
-        return None
-    elif comp == "rock":
-        return user == "paper"
-    elif comp == "paper":
-
-        return user == "scissors"
-    elif comp == "scissors":
-        return user == "rock"
-    
-
-def score(state):
-    if user_wins(state) == True:
-        state["user_score"] += 1
-    elif user_wins(state) == False:
-        state["comp_score"] += 1
-    
-    # Show picks and display the score
-    show_picks(state)
-    display_score(state)
-    
-    # Reset
-    state["comp_pick"] = None
-    state["user_pick"] = None
-    state["choice_index"] = 0
-    
-    display(state)
-
-def display(state):
-    """
-    Displays the shape of the current choice index.    
-    """
-    user_pick = get_user_pick(state)
-
-    if user_pick == "rock":
-        sense.set_pixels(big_rock)
-    elif user_pick == "paper":
-        sense.set_pixels(big_paper)
-    elif user_pick == "scissors":
-        sense.set_pixels(big_scissors)
-
-####
-# Intro on Program Start
-####
-
 sense.clear()
 
-for image in [big_rock, big_paper, big_scissors]:
-    sense.set_pixels(image)
-    time.sleep(.5)
-    
-sense.show_message("<")
-sense.set_rotation(180)
-sense.show_message("<")
-sense.set_rotation(0)
+"""
 
-# Start at the rock
-sense.set_pixels(big_rock)
+  Make your Sense HAT into a Flappy HAT!
+  
+  Help the bird fly though the columns.  Each column is
+  one point.  How many points can you get?
+  
+  Read through the code to learn how the game is made and
+  try changing some of the variables at the top. Turn on
+  debug_mode to print extra information about what the program
+  is doing.
+  
+  Inspired by the Astro Pi team's flappy astronaut!
+  https://www.raspberrypi.org/learning/flappy-astronaut
+
+  Note: Requires sense_hat version 2.2.0 or later
+"""
+
+# Edit these variables to change key game parameters
+col_color = (0,255,0)      # RGB color of column pixels
+bg_color = (0,0,0)         # RGB color of background pixels
+bird_color = (200,200,0)   # RGB color of the bird pixel
+bird_y = 2                 # Where the player starts 
+bird_lives = 3             # How many lives does the bird get?
+columns = [ (7, 3, 3)]     # Starting column x index, gap y start, gap size
+column_speed_base = 1      # Base speed for column movement, from 1 to 5
+debug_mode = False         # Set to true to print extra information
+
+# Variables for convenience and readability
+up_key = sense_hat.DIRECTION_UP
+pressed = sense_hat.ACTION_PRESSED
+
+# Initial settings - should be no need to alter these
+speed = 0
+game_over = False
+setup = True
+moved = False
+column_interval = 10
+
+# Debug functions: pause or print messages if debug_mode is set to True
+def debug_message(message):
+  if debug_mode:
+    print(message)
+
+def debug_pause(message):
+  # Currently not used
+  # To examine a specific portion of the program, add a debug_pause with a message
+  # Or, change an existing debug_message call to debug_pause to pause there
+  if debug_mode:
+    input(str(message) + "  Press enter to continue...")
+    
+# Get true color values for comparisons: get_pixel() sometimes returns different values
+sense.set_pixel(0,0, col_color)
+col_color = sense.get_pixel(0,0)
+sense.set_pixel(0,0, bg_color)
+bg_color = sense.get_pixel(0,0)
+sense.set_pixel(0,0, bird_color)
+bird_color = sense.get_pixel(0,0)
+sense.clear()
+
+# Save setup in a tuple to restart game
+reset_state = (col_color, bg_color, bird_color, bird_y, bird_lives, columns, column_speed_base, speed, game_over)
 
 ####
-# Main Loop
+# Game functions
+####
+
+def move_columns():
+  debug_message("Moving columns")
+  global columns
+  starting_cols = len(columns)
+  
+  # Shift x coordinate of colums to left
+  columns = [(c[0] -1, c[1], c[2]) for c in columns]
+
+  # Add a new column if needed
+  if max([c[0] for c in columns]) == 4:
+    gap_size = randint(2,4)
+    row_start = randint(1 + gap_size, 6)
+    columns.append((7,row_start,gap_size))
+
+def draw_column(col, custom_color = None):
+  debug_message("Drawing column")
+  
+  if custom_color:
+    back_color = custom_color
+  else:
+    back_color = bg_color
+
+  # Construct a list of column color and background color tuples, then set those pixels
+  x, gap_start, gap_size = col
+  c = [col_color] * 8
+  c[gap_start - gap_size: gap_start] = [back_color] * gap_size
+  for y, color in enumerate(c):   
+    sense.set_pixel(x,y,color)
+
+def draw_screen(custom_color = None):
+  debug_message("Drawing screen")
+  
+  if custom_color:
+    back_color = custom_color
+  else:
+    back_color = bg_color
+  
+  sense.clear(back_color)
+  # Filter out offscreen columns then draw visible columns
+  visible_cols = [c for c in columns if c[0] >= 0] 
+  for c in visible_cols:
+    draw_column(c, back_color)  
+
+def draw_bird(falling = True):
+  debug_message("drawing bird")
+  global bird_y, bird_lives, game_over
+  # Replace bird with upcoming background or column at x=4
+  sense.set_pixel(3,bird_y,sense.get_pixel(3, bird_y))
+  
+  if falling:
+    bird_y += speed
+
+  # Stay onscreen
+  if bird_y > 7:
+    bird_y = 7
+  if bird_y < 0:
+    bird_y = 0
+  
+  # Collisions are when the bird moves onto a column
+  hit = sense.get_pixel(3, bird_y) == col_color
+  if hit:
+    flash_screen()
+    bird_lives -= 1
+    # ignore any keypresses here
+    sense.stick.get_events()
+  
+  # Draw bird lives
+  if bird_lives > 8:
+    # Can only draw 8 at a time
+    draw_lives = 8
+  else:
+    draw_lives = bird_lives
+    
+  for i in range(draw_lives):
+    sense.set_pixel(0,i, (200,200,200))
+  
+  game_over = bird_lives < 0
+  
+  # Draw bird in new position
+  sense.set_pixel(3,bird_y,bird_color)
+  debug_message("Bird drawn")
+
+def flash_screen():
+  for i in range(3):
+    custom_color = ([randint(50, x) for x in [255,255,255]])
+    draw_screen(custom_color)
+    # Make sure bird is still visible
+    sense.set_pixel(3,bird_y,bird_color)
+    sleep(.1)
+  draw_screen()
+
+####
+# Main Game Loop
 ####
 
 while True:
-    events = sense.stick.get_events()
-    if events:
-      for event in events:
-        if event.action != 'pressed':
-            #this is a hold or keyup; move on
-            continue
-        if event.direction == 'left':
-            game_state["choice_index"] += 1
-            display(game_state)
-        elif event.direction == 'right':
-            game_state["choice_index"] -= 1
-            display(game_state)
-        elif event.direction == 'middle':
-            # Comp picks randomly
-            game_state["comp_pick"] = random.choice(game_state["picks"])
-            # User picks selected option
-            game_state["user_pick"] = get_user_pick(game_state)
-            score(game_state)
+
+  if setup:
+    col_color, bg_color, bird_color, bird_y, bird_lives, columns, column_speed_base, speed, game_over = reset_state
+    # Initial screen setup
+    last_redraw = round(time(), 1) * column_interval
+    draw_screen()
+    draw_bird()
+    
+    # Clear joystick events
+    sense.stick.get_events()
+    column_speed = int(column_speed_base)
+    
+    setup = False
+  
+  column_tick = round(time(), 1) * column_interval
+
+  if (column_tick % (column_interval - column_speed) == 0) and (column_tick > last_redraw):
+    # Enough time has passed: columns move 
+    debug_message("Tick!")
+    speed = 1
+    # make columns faster if possible as game goes on
+    if column_interval > (column_speed + 2):
+      column_speed = column_speed_base + len(columns) // (column_interval * 3)
+
+    move_columns()
+    draw_screen()
+    draw_bird()
+    debug_message("Tick length: " + str(column_tick - last_redraw) \
+                   + " Speed: " + str(column_speed)\
+                   + "Columns: " + str(len(columns)))
+    last_redraw = column_tick
+
+  events = sense.stick.get_events()
+  if events:
+    for e in events:
+      debug_message("Processing joystick events")
+      if e.direction ==  up_key and e.action == pressed:
+        # User pressed up: move bird up and columns over
+        debug_message("Joystick up press detected")
+        move_columns()
+        draw_screen()
+        speed = -1
+        draw_bird()
+        moved = True
+        # Prevent double falls
+        last_redraw -= column_interval // 2
+  else:
+    moved = False
+    
+  if game_over:
+    flash_screen()
+      
+    # Score is number of columns survived
+    score = len(columns) - 2 
+    sense.show_message(str(score) + "pts!", text_colour=(255,255,255))
+    
+    # Start over
+    setup = True
